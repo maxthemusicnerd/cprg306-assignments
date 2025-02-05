c1eefe5fbd53baf3a54e66311bdd4f3e59f9dac1
maxthemusicnerd <maximoleshko7@gmail.com>
2025-02-04 23:44:11 -0700

    made the text be on seperate lines

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <h3>CPRG 306: Web Development 2 - Assignments</h3>
-      <Link href="/week-2">Week-2 Assignment</Link>
-      <Link href="/week-3">Week-3 Assignment</Link>
+      <Link href="/week-2" className="block">Week-2 Assignment</Link>
+      <Link href="/week-3" className="block">Week-3 Assignment</Link>
     </div>
   );
 }
