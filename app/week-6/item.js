3952dc6b478daa0edd0f463e73f5e400b30a24b2
maxthemusicnerd <maximoleshko7@gmail.com>
2025-03-04 17:06:44 -0700

    fixed the ugly width issue

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -1,7 +1,7 @@
 
 export default function Item({name, quantity, category}) {
     return(
-        <div className="border border-blue-300 p-4 m-8 text-white">
+        <div className="border border-blue-300 p-4 m-8 text-white max-w-sm">
             <div>{name}</div>
             <div>{quantity}</div>
             <div>{category}</div>
